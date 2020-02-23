da224d8a52d08a46e9d398ff7e4a2a7683b6e83f
Briceson Roy <royzone3@ymail.com>
2020-02-23 16:29:40 -0500

    First Commit

---
+++ ww2-js-eventhandlers.js
@@ -0,0 +1,173 @@
+// base line values
+let currentQuestion = 0;
+let currentOptions = 0;
+let currentScore = 0
+
+//handles updating the score
+
+function resetScoreBoard() {
+  currentQuestion = 0
+  currentScore = 0
+  $('.score-number').text(currentScore)
+  $('.question-number').text(currentQuestion)
+}
+
+function updateCurrentScore() {
+  currentScore++;
+  $('.score-number').text(currentScore)
+}
+
+function updateQuestionNumber() {
+  currentQuestion++;
+  $('.question-number').text(currentQuestion)
+}
+
+//01 When page loads do this
+function handleQuizApp() {
+  beginQuiz();
+  restartQuiz()
+  // displayQuestions(currentQuestion);
+  // displayOptions(currentOptions);
+  // checkAnswerScore();
+  // endOfLine();
+
+}
+
+
+//02 when a user clicks on start quiz button 
+function beginQuiz() {
+  $('.begin-quiz').on('click', function () {
+    displayQuestions(currentQuestion);
+    displayOptions(currentOptions);
+    checkAnswerScore();
+    $('.score-number').text(0);
+    $('.introduction').hide();
+    $('.begin-quiz').hide()  
+  })
+}
+
+//03 displays the question set
+function displayQuestions(currentQuestion) {
+
+  const htmlOutput = $(`<form class = "block-this">
+      <fieldset id= "answer-pool">
+        <legend class= "question">${STORE[currentQuestion].question}</legend>
+      </fieldset>
+<button class= "submission">CHARGE!</button>
+    </form>`)
+  $('.block-this').html(htmlOutput)
+}
+
+// //04 Displays the options for the current question and submission button.
+
+
+function displayOptions(currentOptions) {
+  STORE[currentQuestion].answers.forEach(function (answerLabel) {
+    $("#answer-pool").append(`<input type="radio" name="answer" id="${answerLabel} " value="${answerLabel}">${answerLabel}</input>`);
+  });
+}
+
+
+//05 checks whether the chosen option is right or wrong and displays respective message
+function checkAnswerScore() {
+  console.log('checkAnswerScore')
+  $(document).on('click', '.submission', function (event) {
+    event.preventDefault();
+    let userAnswer = $("input[name='answer']:checked").val();
+    console.log(userAnswer)
+    if (userAnswer === undefined) {
+      let message = 'Hold on a second. Choose an Answer.';
+      $('.block-this').append(message)
+    }
+    else {
+      if (userAnswer === STORE[currentQuestion].correctAnswer) {
+        updateCurrentScore()
+        let output = null;
+        output = `<section class = "right-answer">
+       <h3>They made it through!</h3>
+       <img src="images/right-answer.jpg" alt="squad made it through!" class="images" width="200px">
+       <input type = "hidden" class= "currentQuestion" value = "${currentQuestion}" />
+      <button class ="next">
+      Next group
+      </button>
+      </section>`;
+        $('.block-this').html(output);
+      }
+      else {
+        let output = null;
+        output = `<section class= "wrong-answer">
+       <h1>We've got a squad down!</h1>
+       <img src="images/wrong-answer.jpg" alt="we have a man down!" class="images" width="200px">
+       <h2 class= "correction">${STORE[currentQuestion].correctAnswer}</h2>
+       <input type = "hidden" class= "currentQuestion" value = "${currentQuestion}" />
+       <button class ="next">
+       Next group
+       </button>
+       </section>`
+        $('.block-this').html(output);
+      }
+      $('.block-this').hide()
+      $('#answer-pool').hide()
+      $('submission').remove()
+      $('.block-this').show();
+      updateQuestionNumber()
+    }
+  })
+}
+
+
+$(document).on('click', '.next', function (event) {
+  event.preventDefault();
+  $('.right-answer').hide();
+  $('.wrong-answer').hide();
+  let currentQuestionNum = $(this).parent().find('.currentQuestion').val()
+  console.log(currentQuestionNum)
+  //if final question do not incriment!
+  currentQuestionNum++
+  if (currentQuestionNum < STORE.length) {
+    displayQuestions(currentQuestionNum++);
+    displayOptions(currentQuestionNum++)
+  }
+  else {
+    endOfLine()
+  }
+})
+
+
+//06 checks whether it reached the end of questions list 
+function endOfLine() {
+  if (currentScore <= 5) {
+    ending = $(`<p>Though the day has been won, many men have lost their lives due to your lack of Knowledge. </p>
+    <img src="images/bad-ending.jpg" alt="Medals of Honor for all." class="images" width="200px">
+      <button class = "retry-quiz">Retake Quiz</button>
+    </section>`)
+
+  }
+  else if (currentScore >= 6) {
+    ending = $(`<p>You have used your intelligence wisely and your unit has minimized the casualties. <strong>Congradulations, commander!</strong></p>
+           <img src="images/good-ending.jpg" alt="Medals of Honor for all." class="images" width="200px">
+      <button class = "retry-quiz">Retake Quiz</button>`)
+  }
+  $('.block-this').html(ending)
+}
+
+
+//07 handles restarting the quiz and score
+function restartQuiz() {
+  $(document).on('click', '.retry-quiz', function (event) {
+    event.preventDefault();
+    resetScoreBoard();
+    $('.block-this').html('<p class = "introduction"> Imagine you are a commander during the invasion of Normandy. Knowledge is important to keep your men alive. Keep at least 6 landing ships alive to get the good ending. <strong>Good luck commander</strong>.</p><button class= "begin-quiz">Begin</button>');
+    beginQuiz();
+    $('.introduction').show();
+    $(`.begin-quiz`).show();
+    console.log('reset')
+  })
+}
+
+
+
+
+
+
+$(handleQuizApp);
---
+++ ww2-js-store.js
@@ -0,0 +1,112 @@
+const STORE = [
+    {
+      question: 'This treaty saw the abdication of Kaiser Wilhelm I and the downsizing of the German army following their defeat in World War I.',
+      answers: [
+        'Treaty of Prussia',
+        'Treaty of Versailles',
+        'Treaty of Bruges',
+        'Treaty of Weimar'
+      ],
+      correctAnswer:
+        'Treaty of Versailles'
+    },
+    {
+      question:
+        'This was the name of the provisional government set up by the Allies following the German defeat in World War I.',
+      answers: [
+        'Weimar Republic',
+        'Prussian Republic',
+        'Wilhelm Republic',
+        'Versailles Republic'
+      ],
+      correctAnswer:
+        'Weimar Republic'
+    },
+    {
+      question:
+        'This country was the birthplace of Adolf Hitler.',
+      answers: [
+        'Germany',
+        'Poland',
+        'Austria',
+        'Hungary'
+      ],
+      correctAnswer: 'Austria'
+    },
+    {
+      question: 'During his imprisonment, Adolf Hitler wrote this manifesto which would hold all of his core politics and ideologies.',
+      answers: [
+        'Sturm und Drang',
+        'Der Frieschutz',
+        'Mein Kampf',
+        'Liebe-Hutten'
+      ],
+      correctAnswer: 'Mein Kampf'
+    },
+    {
+      question:
+        'In his rise to power Hitler had a strong hatred for this ethnic group and blamed all of Germany’s current problems on them.',
+      answers: [
+        'Gentiles',
+        'Jews',
+        'Muslims',
+        'Aryans'
+      ],
+      correctAnswer:
+        'Jews'
+    },
+    {
+      question: 'This was the name of the German Secret police set up by the Third Reich to weed out enemies of the state.',
+      answers: [
+        'Gestapo',
+        'FAMAS',
+        'Purgis',
+        'FARC'
+      ],
+      correctAnswer: 'Gestapo'
+    },
+    {
+      question:
+        'This word in German means "Lightning War" and it was also the name for the German’s attack on London.',
+      answers: [
+        'Schadenfreude',
+        'Blitzkrieg',
+        'Eisenreich',
+        'Panzer'
+      ],
+      correctAnswer:
+        'Blitzkrieg'
+    },
+    {
+      question: 'This was the name of the German Air Force under the command of Herman Goring which was instrumental during the attack on London.',
+      answers: [
+        'Schnieder',
+        'Panzer',
+        'Luftwaffe',
+        'Wehrmacht'
+      ],
+      correctAnswer:
+        'Luftwaffe'
+    },
+    {
+      question: 'During Operation Barbarossa, this major Russian city was the main target and the site of the Nazi’s defeat in the Soviet Union.',
+      answers: [
+        'Moscow',
+        'Leningrad',
+        'Stalingrad',
+        'St. Petersburg'
+      ],
+      correctAnswer: 'Stalingrad'
+    },
+    {
+      question:
+        'This man was the leader of the infamous Afrikaan Panzer Corps whose shrewd tactics with tanks earned him the alias Desert Fox.',
+      answers: [
+        'Herman Goring',
+        'Rudolph Hess',
+        'Erwin Rommel',
+        'Heinrich Himmler'
+      ],
+      correctAnswer: 'Erwin Rommel'
+    }
+  ];
